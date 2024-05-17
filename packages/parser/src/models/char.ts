aa06f8b34b14bbffc1f3c49c1670d38ea6aa8105
hahnlee <han@peulda.com>
2024-05-17 14:06:42 +0900

    feat(parser): 기본 Hypen 변환 추가

--- packages/parser/src/models/char.ts
+++ packages/parser/src/models/char.ts
@@ -50,7 +50,12 @@ export class CharControl {
   constructor(public control: CharControls) {}
 
   toString() {
-    return ''
+    switch (this.control) {
+      case CharControls.Hyphen:
+        return '-'
+      default:
+        return ''
+    }
   }
 }
 
